02d5a4e7bd3789e44bb8830c2f3ac533e43999d3
Alline Franciely <alline_franciely@hotmail.com>
2023-08-24 20:20:14 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/GoogleSheetsHelper.cs
@@ -0,0 +1,40 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Http;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+
+namespace WebApplication1
+{
+    public class GoogleSheetsHelper
+    {
+        public SheetsService Service { get; set; }
+        const string APPLICATION_NAME = "ToDoList";
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+
+        public GoogleSheetsHelper()
+        {
+            InitializeService();
+        }
+
+        private void InitializeService()
+        {
+            var credential = GetCredentialsFormFile();
+            Service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = APPLICATION_NAME
+            });
+        }
+
+        private GoogleCredential GetCredentialsFormFile()
+        {
+            GoogleCredential credential;
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+            }
+
+            return credential;
+        }
+    }
+}
